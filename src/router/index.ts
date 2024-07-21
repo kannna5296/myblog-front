c59a07197b5213a72edc91d4fb6f583cea0ac229
kannna5296 <kannnahacks5296@gmail.com>
2024-07-21 11:09:10 +0900

    遅延ロードを入れる

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,34 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../components/UserLogin.vue";
-import Register from "../components/UserRegister.vue";
-import PostList from "../components/PostList.vue";
-import PostDetail from "../components/PostDetail.vue";
-import CreatePost from "../components/CreatePost.vue";
-import EditPost from "../components/EditPost.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
 
 const routes = [
-  { path: "/", component: HelloWorld },
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  { path: "/posts", component: PostList },
-  { path: "/posts/:id", component: PostDetail },
-  { path: "/posts/create", component: CreatePost },
-  { path: "/posts/edit/:id", component: EditPost },
+  {
+    path: "/",
+    component: () => import("@/components/HelloWorld.vue"),
+  },
+  {
+    path: "/login",
+    component: () => import("@/components/UserLogin.vue"),
+  },
+  {
+    path: "/register",
+    component: () => import("@/components/UserRegister.vue"),
+  },
+  {
+    path: "/posts",
+    component: () => import("@/components/PostList.vue"),
+  },
+  {
+    path: "/posts:id",
+    component: () => import("@/components/PostDetail.vue"),
+  },
+  {
+    path: "/posts/create",
+    component: () => import("@/components/CreatePost.vue"),
+  },
+  {
+    path: "/posts/edit/:id",
+    component: () => import("@/components/EditPost.vue"),
+  },
 ];
 
 const router = createRouter({
