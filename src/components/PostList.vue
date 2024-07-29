8ee53bd81a1aa11bc5b2bfdb0c433c8afdd65ea6
kannna5296 <kannnahacks5296@gmail.com>
2024-07-29 23:25:26 +0900

    BE通信OK

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,35 +1,35 @@
-<template>
-  <div>
-    <h2>Posts</h2>
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <router-link :to="`/posts/${post.id}`">{{ post.title }}</router-link>
-      </li>
-    </ul>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import axios from "axios";
+import { ref } from 'vue';
+import axiosInstance from '@/router/axios'; // 作成したaxiosインスタンスをインポート
 
 interface Post {
-  id: number;
+  postId: string;
   title: string;
 }
 
 const posts = ref<Post[]>([]);
 
-const fetchPosts = async () => {
-  try {
-    const response = await axios.get("/api/post");
-    posts.value = response.data;
-  } catch (error) {
-    console.error("Failed to fetch posts", error);
-  }
+const fetchPosts = () => {
+  axiosInstance.get('/api/post')
+    .then(response => {
+      posts.value.push(response.data);
+    })
+    .catch(error => {
+      console.error('Failed to fetch posts', error);
+    });
 };
 
-onMounted(() => {
-  fetchPosts();
-});
+fetchPosts();
 </script>
+
+<template>
+  <div>
+    <h2>Posts</h2>
+    <ul>
+      <li v-if="posts.length === 0">Loading...</li>
+      <li v-for="post in posts" :key="post.postId">
+        <router-link :to="`/post/${post.postId}`">{{ post.title }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
