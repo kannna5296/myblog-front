0c46379e7baf3adbce9bb0c62283e79049fc501f
kannna5296 <kannnahacks5296@gmail.com>
2024-10-17 09:19:30 +0900

    ヘッダー処理共通化

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { OpenAPI } from './repositories/generated/core/OpenAPI';
 
 createApp(App).use(store).use(router).mount('#app');
+
+// トークンをヘッダーに追加する設定
+// 別ファイルにしたい
+OpenAPI.HEADERS = async () => {
+  const token = localStorage.getItem('token'); // トークンを取得
+  return {
+    Authorization: `Bearer ${token}`,
+    // TODO CsrfTokenを取得して追加する
+  };
+};
