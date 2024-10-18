b34db6430a028a659981c439108deeee335c5711
kannna5296 <kannnahacks5296@gmail.com>
2024-10-18 09:56:26 +0900

    POST突破用：csrfTokenも追加

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,22 @@ import { OpenAPI } from './repositories/generated/core/OpenAPI';
 createApp(App).use(store).use(router).mount('#app');
 
 // トークンをヘッダーに追加する設定
-// 別ファイルにしたい
+// TODO 別ファイルにしたい
+function getCsrfToken() {
+  console.log(`cookie!!${document.cookie}`);
+  const cookieValue = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('XSRF-TOKEN='))
+    ?.split('=')[1];
+  console.log('嗚呼あ');
+  console.log(`cookie${cookieValue}`);
+  return cookieValue;
+}
+
 OpenAPI.HEADERS = async () => {
   const token = localStorage.getItem('token'); // トークンを取得
   return {
-    Authorization: `Bearer ${token}`,
-    // TODO CsrfTokenを取得して追加する
+    Authorization: token ? `Bearer ${token}` : '',
+    'X-XSRF-TOKEN': getCsrfToken() || '',
   };
 };
