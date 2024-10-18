3814872c9247b29c87a671e7d6ae027e39b1884c
kannna5296 <kannnahacks5296@gmail.com>
2024-10-18 09:58:03 +0900

    要らんログ削除

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,10 @@ createApp(App).use(store).use(router).mount('#app');
 // トークンをヘッダーに追加する設定
 // TODO 別ファイルにしたい
 function getCsrfToken() {
-  console.log(`cookie!!${document.cookie}`);
   const cookieValue = document.cookie
     .split('; ')
     .find((row) => row.startsWith('XSRF-TOKEN='))
     ?.split('=')[1];
-  console.log('嗚呼あ');
-  console.log(`cookie${cookieValue}`);
   return cookieValue;
 }
 
