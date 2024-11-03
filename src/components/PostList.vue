1029c987d83fe0bf16c6fc50c36f5792004ab3e1
kannna5296 <kannnahacks5296@gmail.com>
2024-11-03 17:06:08 +0900

    feat: 要らんdiv修正

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -18,46 +18,41 @@ fetchPosts();
 </script>
 
 <template>
-  <div>
-    <v-container>
-      <div class="text-h4">
-        Posts
-      </div>
-      <v-list>
-        <!-- 各ポストをカードとして表示 -->
-        <v-card
-          v-for="(post, index) in posts"
-          :key="index"
-          class="mb-4"
-          elevation="2"
-          tag="router-link"
-          :to="`/post/${post.postId}`"
-        >
-          <v-card-content>
-            <!-- タイトル部分 -->
-            <v-card-title>
-              {{ post.title }}
-            </v-card-title>
-          </v-card-content>
-        </v-card>
-      </v-list>
-      <v-btn
-        class="mt-2"
-        color="primary"
-        block
-        :to="'/post/create'"
+  <v-container>
+    <div class="text-h4">
+      Posts
+    </div>
+    <v-list>
+      <!-- 各ポストをカードとして表示 -->
+      <v-card
+        v-for="(post, index) in posts"
+        :key="index"
+        class="mb-4"
+        elevation="2"
+        tag="router-link"
+        :to="`/post/${post.postId}`"
       >
-        投稿する
-      </v-btn>
-    </v-container>
-    <li v-if="posts.length === 0">
-      投稿がありません！
-    </li>
-  </div>
+        <v-card-content>
+          <!-- タイトル部分 -->
+          <v-card-title>
+            {{ post.title }}
+          </v-card-title>
+        </v-card-content>
+      </v-card>
+    </v-list>
+    <v-btn
+      class="mt-2"
+      color="primary"
+      block
+      :to="'/post/create'"
+    >
+      投稿する
+    </v-btn>
+  </v-container>
+  <li v-if="posts.length === 0">
+    投稿がありません！
+  </li>
 </template>
 
 <style lang="css">
-ul {
-  list-style: none;
-}
 </style>
