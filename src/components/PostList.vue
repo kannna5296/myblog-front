55260080c1f7be81ebfd24c22a25c2c69fbfd84a
kannna5296 <kannnahacks5296@gmail.com>
2024-11-15 18:49:08 +0900

    fix: タイトル列にリンクを貼る

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -4,18 +4,17 @@ import { PostRepository } from '@/repositories/generated/services/PostRepository
 import { PostIndexResponse } from '@/repositories/generated';
 
 // TODO 401だったらログイン画面にリダイレクト。共通処理として作る
-const posts = ref<PostIndexResponse[]>([]);
+const items = ref<PostIndexResponse[]>([]);
 
 const fetchPosts = async () => {
   await PostRepository.index().then((response) => {
-    posts.value = response;
+    items.value.push(...response);
   }).catch((error) => {
     console.error('Failed to fetch posts', error);
   });
 };
 
 fetchPosts();
-
 const headers = [
   { align: 'center', title: 'タイトル', value: 'title' } as const,
   { align: 'center', title: 'ID', value: 'postId' } as const,
@@ -24,24 +23,41 @@ const headers = [
 
 <template>
   <v-container>
-    <div class="text-h4">
-      投稿
-    </div>
-    <v-data-table
-      :headers="headers"
-      :items="posts"
-      class="elevation-1"
-    />
-    <v-btn
-      class="mt-2"
-      color="primary"
-      block
-      :to="'/post/create'"
+    <v-card
+      title="投稿一覧"
+      flat
     >
-      投稿する
-    </v-btn>
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        class="elevation-1"
+      >
+        <template #item="{ item }">
+          <tr>
+            <td>
+              <router-link
+                :to="{ name: 'post Detail', params: { id: item?.postId } }"
+                class="link-style"
+              >
+                {{ item.postId }}
+              </router-link>
+            </td>
+            <td>{{ item.postId }}</td>
+          </tr>
+        </template>
+      </v-data-table>
+
+      <v-btn
+        class="mt-2"
+        color="primary"
+        block
+        :to="'/post/create'"
+      >
+        投稿する
+      </v-btn>
+    </v-card>
   </v-container>
-  <li v-if="posts.length === 0">
+  <li v-if="items.length === 0">
     投稿がありません！
   </li>
 </template>
@@ -60,4 +76,15 @@ const headers = [
   border-bottom: 1px solid #c4c4c7; /* ヘッダの縁線を太く */
 }
 /* border-bottom: thin solid rgba(var(--v-border-color), var(--v-border-opacity));が邪魔なので消したい */
+
+.link-style {
+  color: blue; /* 青文字 */
+  text-transform: uppercase; /* 大文字 */
+  text-decoration: none; /* リンクの下線を消す */
+}
+
+.link-style:hover {
+  text-decoration: underline; /* ホバー時に下線 */
+}
+
 </style>
